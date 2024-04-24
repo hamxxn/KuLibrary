cf26a9834f49aefced1b7d100cda4f8898adea72
hamxxn <150767569+hamxxn@users.noreply.github.com>
2024-04-24 17:28:17 +0900

    Update SeatManager.java

--- src/SeatManager.java
+++ src/SeatManager.java
@@ -1,2 +1,13 @@
 public class SeatManager {
+   ArrayList<Seat> seats=new ArrayList<>();
+    void printSeat(){
+        System.out.println("잔여 좌석입니다: ");
+        Iterator<Seat> iterator=seats.iterator();
+        if(iterator.hasNext()){
+            Seat seat=iterator.next();
+            if(seat.isReserved()){
+                System.out.print(seat.getSeatNumber()+" ");
+            }
+        }
+    }
 }
